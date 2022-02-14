00739292dd2672cc4915d7356854c3dd35815756
mbthiele <martin.thielens@ugent.be>
2022-02-14 16:48:46 +0100

    Error message from the new code has been corrected in part.f90

--- Source/part.f90
+++ Source/part.f90
@@ -3227,8 +3227,7 @@ SPECIES_LOOP: DO Z_INDEX = 1,N_TRACKED_SPECIES
 			PART_HEAT_TRANSFER_MODEL_SELECT: SELECT CASE(LPC%PART_HEAT_TRANSFER_MODEL)
 			
 			CASE(ISOTHERMAL_MODEL)
-					WRITE(MESSAGE,'(A,I0,A)') 'Good job! The default isothermal heat transfer model has correctly been selected ', N,&
-								  'Now you still have to write the code'
+					WRITE(MESSAGE,'(A,I0,A)') 'Good job! The default isothermal heat transfer model has correctly been selected. Write the code now.'
 					CALL SHUTDOWN(MESSAGE) ; RETURN
                      A_COL(1) = 1._EB+DTGOG
                      B_COL(1) = -(DTGOG+DADYDTHVHL)
@@ -3242,8 +3241,7 @@ SPECIES_LOOP: DO Z_INDEX = 1,N_TRACKED_SPECIES
 
 			!Martin
 			CASE(TWO_ZONE_MODEL)
-					WRITE(MESSAGE,'(A,I0,A)') 'Good job! The Two-Zone heat transfer model has correctly been selected ', N,&
-								  'Now you still have to write the code'
+					WRITE(MESSAGE,'(A,I0,A)') 'Good job! The Two-Zone heat transfer model has correctly been selected. Write the code now.'
 					CALL SHUTDOWN(MESSAGE) ; RETURN
 
                   CASE(2) ! Const Temp Wall
