2382e148df4a95ac8c7b61beab6ccf84ec648c1d
mbthiele <martin.thielens@ugent.be>
2022-03-03 15:49:47 +0100

    Save TMP_in and use it next time step

--- Source/type.f90
+++ Source/type.f90
@@ -54,6 +54,7 @@ TYPE LAGRANGIAN_PARTICLE_CLASS_TYPE
    REAL(EB) :: GAMMA                      !< Parameter in Rosin-Rommler distribution
    REAL(EB) :: KILL_RADIUS                !< Radius (m) below which particle is killed (removed)
    REAL(EB) :: TMP_INITIAL                !< Initial temperature (K) of the particles
+   REAL(EB) :: TMP_INNER_INITIAL	  !< Initial inner layer temperature (K) of the particles for the Two-Zone Model ! Martin
    REAL(EB) :: SIGMA                      !< Parameter in Rosin-Rammler distribution
    REAL(EB) :: VERTICAL_VELOCITY          !< Vertical velocity component (m/s) of particle stuck to a solid surface
    REAL(EB) :: HORIZONTAL_VELOCITY        !< Horizontal velocity component (m/s) of particle stuck to a solid surface
@@ -241,8 +242,8 @@ TYPE BOUNDARY_ONE_D_TYPE
    REAL(EB) :: T_IGN=0._EB           !< Ignition time (s)
    REAL(EB) :: TMP_F                 !< Surface temperature (K)
    REAL(EB) :: TMP_F_OLD             !< Holding value for surface temperature (K)
-   REAL(EB) :: TMP_INNER             !< Inner layer temperature for Two-Zone Model (K) Martin
-   REAL(EB) :: TMP_INNER_OLD         !< Holding value Inner layer temperature for Two-Zone Model (K) Martin
+   !REAL(EB) :: TMP_INNER             !< Inner layer temperature for Two-Zone Model (K) Martin
+   !REAL(EB) :: TMP_INNER_OLD         !< Holding value Inner layer temperature for Two-Zone Model (K) Martin
    REAL(EB) :: TMP_B                 !< Back surface temperature (K)
    REAL(EB) :: U_NORMAL=0._EB        !< Normal component of velocity (m/s) at surface, start of time step
    REAL(EB) :: U_NORMAL_S=0._EB      !< Estimated normal component of velocity (m/s) at next time step
